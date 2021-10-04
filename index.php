fc1267f3c5cbb64f97fa0b00271f2b529860fa42
Gabriel Said <gabo.rocke@gmail.com>
2021-10-04 10:11:28 -0500

    Estructura php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>prueba</title>
+  </head>
+  <body>
+    <?php
+      echo "Hola mundo";
+
+     ?>
+  </body>
+</html>
